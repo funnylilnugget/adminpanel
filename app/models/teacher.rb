59163e8d698d6a79f6cc1ddbfc9f44b3c49959fc
funnylilnugget <kenny@funnylilnugget.com>
2018-12-21 17:02:53 -0500

    added cohort edit and validates

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,6 @@
 class Teacher < ApplicationRecord
-  validates :first_name, :last_name, :age, :salary, :education, :hero_name, :quirk, presence: true
+  validates :first_name, :last_name, :education, :hero_name, :quirk, presence: true
+  validates :age, inclusion: {in: 1..150}
+  validates :salary, numericality: {other_than: 0}
   belongs_to :cohort, optional: true
 end
